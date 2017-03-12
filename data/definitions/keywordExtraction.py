fc6ecef2fb92dacdf0238792cc1c0104673d0d2e
Ambareesh Revanur <ambareesh.r@gmail.com>
2017-03-12 23:51:01 +0530

    archive definitions

--- data/definitions/keywordExtraction.py
+++ data/definitions/keywordExtraction.py
@@ -11,7 +11,7 @@ def rakeUP():
 	outputFile = 'oriList.csv'
 	inputFile = 'ScrapedData.txt'
 
-	rake_object = rake.Rake("rake/SmartStoplist.txt", 4, 1, 2)
+	rake_object = rake.Rake("rake/SmartStoplist.txt", 3, 1, 1)
 	sample_file = open(inputFile, 'r')
 	text = sample_file.read()
 	keywords = rake_object.run(text)
